e3ade64e0ee3fbdea39997e16fa844fe59c8714d
Tulika Chowdhury <Tulika.Chowdhury@hyland.com>
2024-12-09 19:51:51 +0530

    Connected to backend to adminoptions

---
+++ src/app/admin-options.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { OptionModel } from './model/OptionModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminOptionsService {
+
+  apiURL = 'http://localhost:5016/Admin/Types';
+
+  constructor(private http : HttpClient) { }
+
+  getOptions():Observable<OptionModel[]> {
+    return this.http.get<OptionModel[]>(this.apiURL);
+  }
+
+  addOption(name : string, colour : string): Observable<number> {
+    return this.http.post<number>(this.apiURL, {name, colour});
+  }
+}
--- src/app/options-viewer/options-viewer.component.ts
+++ src/app/options-viewer/options-viewer.component.ts
@@ -1,11 +1,15 @@
 import {
   Component,
-  OnInit
+  ElementRef,
+  OnInit,
+  ViewChild
 } from '@angular/core';
 import {
   RouterOutlet,
   ActivatedRoute
 } from '@angular/router';
+import { AdminOptionsService } from '../admin-options.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-options-viewer',
@@ -16,12 +20,38 @@ import {
 })
 export class OptionsViewerComponent implements OnInit {
   isActionCreate = true;
-  constructor(private route: ActivatedRoute) {
+
+@ViewChild('name') 
+nameInput: ElementRef = new ElementRef(null);
+
+@ViewChild('colour')
+colourInput: ElementRef = new ElementRef(null);
+
+  constructor(private route: ActivatedRoute, private adminOptionsService: AdminOptionsService, private toastr: ToastrService) {
 
   }
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.isActionCreate = params['action'].toLowerCase() === 'create';
+    });
+  }
+
+  save() {
+    let optionName: string = this.nameInput.nativeElement.value;
+    let optionColour: string = this.colourInput.nativeElement.value;
+    console.log(optionName);
+    console.log(optionColour);
+
+    this.adminOptionsService.addOption(optionName, optionColour).subscribe(data => {
+      console.log(data);
+      this.toastr.success('Option added successfully');
+    });
+  }
+
+  getAllOptions(){
+    console.log('Get all options');
+    this.adminOptionsService.getOptions().subscribe(data => {
+      console.log(data);
     });
   }
 }
